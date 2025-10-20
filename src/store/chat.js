1e9f3440ceae35d3500b80496260d76184ef385f
Nicolas Chabloz <synn.void@gmail.com>
2025-10-20 19:18:04 +0200

    Add .env

--- src/store/chat.js
+++ src/store/chat.js
@@ -5,4 +5,7 @@ export const isAuth = ref(false);
 export const users = ref([]);
 export const allMsg = ref([]);
 export const showUsersList = ref(false);
-export const ws = new WSClient('ws://localhost:8888');
+
+const wsHost = import.meta.env.VITE_WS_HOST || 'localhost';
+const wsPort = import.meta.env.VITE_WS_PORT || '8888';
+export const ws = new WSClient(`ws://${wsHost}:${wsPort}`);
