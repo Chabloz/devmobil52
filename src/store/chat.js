ccb9e1202bf2074c818a9d9b25b6e3cacbc7225c
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-11-03 11:09:32 +0100

    Add .env

--- src/store/chat.js
+++ src/store/chat.js
@@ -6,6 +6,7 @@ export const users = ref([]);
 export const allMsg = ref([]);
 export const showUsersList = ref(false);
 
-const wsHost = import.meta.env.VITE_WS_HOST || 'localhost';
-const wsPort = import.meta.env.VITE_WS_PORT || '8888';
-export const ws = new WSClient(`ws://${wsHost}:${wsPort}`);
+const wsHost = import.meta.env.VITE_WS_HOST ?? 'localhost';
+const wsPort = import.meta.env.VITE_WS_PORT ?? '8888';
+const wsProtocol = import.meta.env.VITE_WS_PROTOCOL ?? 'wss';
+export const ws = new WSClient(`${wsProtocol}://${wsHost}:${wsPort}`);
