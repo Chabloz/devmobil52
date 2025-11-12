446a68023c75e24af7877b3b978c4a50921f22a7
Nicolas Chabloz <43348620+Chabloz@users.noreply.github.com>
2025-11-12 09:40:10 +0100

    Refactor auto-login and connection handling

--- src/AppChat.vue
+++ src/AppChat.vue
@@ -1,18 +1,23 @@
 <script setup>
-  import { onMounted, ref } from 'vue';
-  import { useQuasar } from 'quasar';
-  import { isAuth, ws, users, allMsg } from '@/store/chat.js';
-  import TheChatToolbar from './components/TheChatToolbar.vue';
-  import TheLoginPage from './components/TheLoginPage.vue';
-  import TheChatForm from './components/TheChatForm.vue';
-  import TheChatMessagesList from './components/TheChatMessagesList.vue';
-  import TheChatUsersList from './components/TheChatUsersList.vue';
-  import { connectToChat } from '@/store/chat.js';
+import { onMounted, ref } from 'vue';
+import { useQuasar } from 'quasar';
+import { isAuth, ws, users, allMsg } from '@/store/chat.js';
+import TheChatToolbar from './components/TheChatToolbar.vue';
+import TheLoginPage from './components/TheLoginPage.vue';
+import TheChatForm from './components/TheChatForm.vue';
+import TheChatMessagesList from './components/TheChatMessagesList.vue';
+import TheChatUsersList from './components/TheChatUsersList.vue';
+import { connectToChat } from '@/store/chat.js';
 
-  const $q = useQuasar();
+const $q = useQuasar();
+const tryingAutoLogin = ref(true);
 
-  ws.on('close', () => {
-    if (isAuth.value) {
+ws.on('close', async () => {
+  if (isAuth.value) {
+    try {
+      await connectToChat();
+      return;
+    } catch {
       $q.notify({
         type: 'negative',
         message: 'Connection to server lost !',
@@ -20,20 +25,22 @@
         position: 'top',
       });
     }
-    isAuth.value = false;
-    users.value = [];
-    allMsg.value = [];
-  });
+  }
 
-  const tryingAutoLogin = ref(true);
-  connectToChat()
-    .catch(() => {}) // silently fail if no valid JWT
-    .finally(() => tryingAutoLogin.value = false);
+  isAuth.value = false;
+  users.value = [];
+  allMsg.value = [];
+});
+
+connectToChat()
+  .catch(() => {})
+  .finally(() => {
+    tryingAutoLogin.value = false;
+  });
 </script>
 
 <template>
   <q-layout view="hHh lpr lFf">
-
     <q-header :class="{ 'no-shadow': $q.dark.isActive }" :elevated="!$q.dark.isActive">
       <TheChatToolbar />
     </q-header>
@@ -43,8 +50,9 @@
     <q-page-container>
       <q-page v-if="!tryingAutoLogin" padding :class="{ 'no-scroll': !isAuth }">
         <TheLoginPage v-if="!isAuth" />
-        <TheChatMessagesList v-if="isAuth" />
+        <TheChatMessagesList v-else />
       </q-page>
+
       <q-page v-if="tryingAutoLogin" class="flex flex-center">
         <q-spinner size="50px" color="primary" />
       </q-page>
@@ -53,26 +61,22 @@
     <q-footer v-if="isAuth" class="q-pa-xs" :class="{ 'bg-dark': $q.dark.isActive, 'bg-grey-2': !$q.dark.isActive }">
       <TheChatForm />
     </q-footer>
-
   </q-layout>
 </template>
 
 <style scoped>
-  .q-page-container {
-    max-height: 100vh;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .q-page {
-    overflow-y: auto;
-  }
-
-  .no-scroll {
-    overflow: hidden;
-  }
-
-  .no-shadow {
-    box-shadow: none;
-  }
+.q-page-container {
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.q-page {
+  overflow-y: auto;
+}
+.no-scroll {
+  overflow: hidden;
+}
+.no-shadow {
+  box-shadow: none;
+}
 </style>
